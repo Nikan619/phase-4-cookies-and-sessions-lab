427c20e1f3cd27d56093a85c79150be379498a69
Nikan <njavadzadeh@gmail.com>
2022-12-22 08:43:47 -0800

    commit tree 3207fa216dec2ae7c276a6d5bc61d4f1af391c0c

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,15 @@ class ArticlesController < ApplicationController
   def index
     articles = Article.all.includes(:user).order(created_at: :desc)
     render json: articles, each_serializer: ArticleListSerializer
+
   end
 
   def show
     article = Article.find(params[:id])
     render json: article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+    
   end
 
   private
